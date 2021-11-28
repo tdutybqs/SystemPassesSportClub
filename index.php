2828badc376a5987368859979d6774452a92e0b4
danyu <kasyanova.b@effective-group.ru>
2021-11-28 17:15:07 +0300

    future3d3

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
-echo "Check";
+$pathToCustomers = __DIR__ . '/Jsons/customers.json';
+$customersTxt = file_get_contents($pathToCustomers);
+$customers = json_decode($customersTxt, true);
+
+$customerIdToInfo = [];
+foreach ($customers as $customer => $customer){
+    $customerIdToInfo[$customer['id']] = $customer;
+}
+
+$pathToPasses = __DIR__ . '/Jsons/passes.json';
+$passesTxt = file_get_contents($pathToPasses);
+$passes = json_decode($passesTxt, true);
+
+if ('/passes' === $_SERVER['PATH_INFO']) {
+    $customerIdToInfo = [];
+    foreach ($customers as $customer){
+        $customerIdToInfo[$author['id']] = $customer;
+    }
+}
+$httpCode = 200;
+$result = [];
